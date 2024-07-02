3086270f2a836ca9a02d34f137865ff1973e073b
Sanagiig <laiwenjunhhh@163.com>
2024-07-02 08:03:23 +0800

    init

---
+++ apps/app/internal/logic/order_list_logic.go
@@ -0,0 +1,31 @@
+package logic
+
+import (
+	"context"
+
+	"app/internal/svc"
+	"app/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type OrderListLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// 订单列表
+func NewOrderListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *OrderListLogic {
+	return &OrderListLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *OrderListLogic) OrderList(req *types.OrderListRequest) (resp *types.OrderListResponse, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
